51dde9b7f1609c2830151f0054d65ece00c9b66f
Aman Tuladhar <Aman.Tuladhar@pointclickcare.com>
2016-03-30 14:37:47 +0545

    Fixed the state change issue directly. Changed the background

--- js/main.js
+++ js/main.js
@@ -35,61 +35,6 @@
             wheelSpeed: 0.9
         });
 
-//==============___Menu & Pages Animation___================
-
-        var linkHome = 0;
-        var linkPage = '';
-
-        function pageOn() {
-            $('#main-menu').addClass('main-menu-pgactive');
-            $('#section-home').addClass('section-vcardbody-pgactive');
-            $('.profileActive').removeClass('profileActive');
-            $('#profile2').addClass('profileActive');
-            $('.section-vcardbody').perfectScrollbar({
-                wheelSpeed: 0.9
-            });
-
-            linkHome = 1;
-        }
-
-        function pageOff() {
-            $('.section-page-active').removeClass('section-page-active');
-            $('#main-menu').removeClass('main-menu-pgactive');
-            $('#section-home').removeClass('section-vcardbody-pgactive');
-            $('.profileActive').removeClass('profileActive');
-            $('#profile1').addClass('profileActive');
-            linkHome = 0;
-        }
-
-
-        $(".link-page").on('click', function (event) {
-            // event.preventDefault();
-            var x = this;
-            window.setTimeout(function(){
-                var linkPage = '#' + $(x).attr('ui-sref');
-                $('.menuActive').removeClass('menuActive');
-                $(x).addClass('menuActive');
-                $('.section-page-active').removeClass('section-page-active');
-                $(linkPage).addClass('section-page-active');
-                pageOn();
-            },300)
-
-        });
-
-
-        $(".link-home").on('click', function (event) {
-            // event.preventDefault();
-
-            if (linkHome == 0) {
-                //pageOn();
-            }
-            else if (linkHome == 1) {
-                $('.menuActive').removeClass('menuActive');
-                $(this).addClass('menuActive');
-                pageOff();
-            }
-        });
-
 //==============___Blog - Ajax___================
         function loadPost() {
             $.ajax({
